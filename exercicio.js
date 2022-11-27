8944a6fb4d53ed77110de4d3a8063eb1433de9cd
Brena <brenaennet@gmail.com>
2022-11-26 22:55:08 -0300

    finalizando os exercícios

--- exercicio.js
+++ exercicio.js
@@ -1,5 +1,5 @@
 // 1 Declare uma variável de nome weight
-let weight
+// let weight
 // 2 Que tipo de dado é a variavel acima?
 
 // R: console.log(typeof weight)
@@ -18,24 +18,55 @@ let weight
 // let peso = 56.0
 // let idSubscribed = true
 
- // 4 A variavel student abaixo é de que tipo de dados
+ // 4 A variavel student abaixo é de que tipo de dados?
 
- let student = {
-    name:"Ana",
-    age: 30,
-    peso: 56.0,
-    idSubscribed: true
- }
- console.log(`A ${student.name} tem ${student.age} anos e pesa ${student.peso} kilos`)
+ // R:Object
+
+//  let student = {
+//     name:"Ana",
+//     age: 30,
+//     peso: 56.0,
+//     idSubscribed: true
+//  }
+//  console.log(`A ${student.name} tem ${student.age} anos e pesa ${student.peso} kilos`)
 
  // 5 Declare uma variavel do tipo array, de nome students e atribua a ela nenhum valor,
  // ou seja, somente o array vazio
-let students = [
+// let students = [
 
-]
+// ]
 
 //6 Reatribua um valor para a variavel acima , colocando dentro dela o objeto student
 // da questão 4(nao copiar e colar o objeto, mas usar o objeto e inserir ele no array)
 
-//PAREI AQUI
+// let students = [
+//     student
+// ]
+// console.log(students)
 
+// 7. Coloque no console o valor da posição zero do array acima
+
+// console.log(students[0])
+
+// 8. Crie um novo student e coloque na posição 1 do array students
+
+// reatribuindo os valores de dois objetos dentro de um Array
+// const jhon = {
+//     name: "Jhon",
+//     age: 30,
+//     peso: 78.0,
+//     idSubscribed: true
+// }
+// students = [
+//     student,
+//     jhon
+// ]
+// console.log(students)
+
+//9. Sem rodar o código abaixo responda qual é a resposta do código abaixo e por que?
+// após sua resposta, rode o código e veja se você acertou
+ 
+console.log(a)
+var a = 1
+
+// A resposta será undefined, porque a variavel não foi definida antes de ser chamada. Haverá um hoisting
