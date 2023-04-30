62ef211712477cf7fd5cd8df47c838fce9180c5a
Brena <bpaiva@uniaoquimica.com.br>
2023-04-30 17:02:31 -0300

    Numbers

--- Console/index.js
+++ Console/index.js
@@ -1,4 +1,18 @@
-var grupo = [1, 2, 3, 4, 5].map(i => {
-    return (i % 2 === 0 ? 'even' : 'odd');
-})
-console.log(grupo)
+// var grupo = [1, 2, 3, 4, 5].map(i => {
+//     return (i % 2 === 0 ? 'even' : 'odd');
+// })
+// console.log(grupo)
+
+///NUMBER
+const av1 = 70;
+const av2 = 80;
+const total = av1 + av2 / 3;
+console.log(total.toFixed(2))
+// toString 2 converte o valor em binário
+console.log(total.toString(2))
+
+// Alguns cuidados com números em javascript
+console.log("10" / 2)
+console.log(0.1 + 0.7)
+
+//Usando Math
